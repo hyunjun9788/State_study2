479e861ed791560b4609124b83afc3c8a2dc1236
hyunjun9788 <namhyunjun9788@gmail.com>
2023-06-26 21:41:57 +0900

    feat: Input.js 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import './App.css'
 import Button from "./components/Button";
+import Input from "./components/Input";
 
 function App() {
     const [inputs, setInputs] = useState(false);
@@ -9,13 +10,15 @@ function App() {
     const buttonClickHandler = () => {
         setInputs(!inputs)
         setButtonText(inputs ? '입력하기' : '입력 취소하기')
+
 };
+
 return (
 
     <div className='main'>
         <Button buttonText={buttonText} onClick = {buttonClickHandler}/>
-        {inputs ? <input className='first' type='text' placeholder='이름을 입력해주세요.'/> : null}
-        {inputs ? <input className='second' type='text' placeholder='나이를 입력해주세요.'/> : null}
+        {inputs ? <Input className='first' type='text' placeholder='이름을 입력해주세요.'/> : null}
+        {inputs ? <Input className='second' type='text' placeholder='나이를 입력해주세요.'/> : null}
     </div>
 
 )
---
+++ src/components/Input.js
@@ -0,0 +1,7 @@
+function Input({className,placeholder}){
+    return(
+        <input className={className} type='text' placeholder={placeholder}/>
+    )
+}
+
+export default Input
