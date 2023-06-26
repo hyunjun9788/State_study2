c7672de88a5695ead972a47e407f50cf6d6b3ed6
hyunjun9788 <namhyunjun9788@gmail.com>
2023-06-26 18:26:59 +0900

    feat: setButtonText 추가, 클릭 시 버튼명 변경

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React, {useState} from 'react';
 import './App.css'
 function App() {
     const [inputs, setInputs] = useState(false);
-
+    const [buttonText, setButtonText] = useState('입력하기')
 
     const buttonClickHandler = () => {
         setInputs(!inputs)
-
+        setButtonText(inputs ? '입력하기' : '입력 취소하기')
 };
-
 return (
 
     <div className='main'>
-        <button onClick={buttonClickHandler}>입력하기</button>
+        <button onClick={buttonClickHandler}>{buttonText}</button>
         {inputs ? <input className='first' type='text' placeholder='이름을 입력해주세요.'/> : null}
         {inputs ? <input className='second' type='text' placeholder='나이를 입력해주세요.'/> : null}
     </div>
